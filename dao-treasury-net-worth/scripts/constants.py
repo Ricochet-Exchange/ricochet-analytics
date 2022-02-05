08290450af7b78fb307b616f7afcde993170a404
MrSquiggles13 <ayresguitar@gmail.com>
2022-02-05 13:50:52 -0500

    Cant get v1.0 or v0 to work with events

---
+++ dao-treasury-net-worth/scripts/constants.py
@@ -0,0 +1,25 @@
+TOKENS = {
+    'dai': '0x1305F6B6Df9Dc47159D12Eb7aC2804d4A33173c2',
+    'ethereum': '0x27e1e4E6BC79D93032abef01025811B7E4727e85',
+    'usd-coin': '0xCAa7349CEA390F89641fe306D93591f87595dc1F',
+    'bitcoin': '0x4086eBf75233e8492F1BCDa41C7f2A8288c2fB92',
+    'maker': '0x2c530aF1f088B836FA0dCa23c7Ea50E669508C4C',
+    'matic-network': '0x3aD736904E9e65189c3000c7DD2c8AC8bB7cD4e3',
+    'sushi': '0xDaB943C03f9e84795DC7BF51DdC71DaF0033382b',
+    'idle': '0xB63E38D21B31719e6dF314D3d2c351dF0D4a9162',
+    'richochet': '0x263026e7e53dbfdce5ae55ade22493f828922965'                                                                                                            
+}
+
+BANKS = {
+    # 'v1.1': '0xe78dC447d404695541b540F2FbB7682fd24d778B',
+    'v1.0': '0xaD39F774A75C7673eE0c8Ca2A7b88454580D7F53',
+    'v0': '0x91093c77720e744F415D33551C2fC3FAf7333c8c'
+}
+
+BANK_BLOCK_START = {
+    'v1.1': 22670647,
+    'v1.0': 22402893,
+    'v0': 21441457
+}
+
+TREASURY_ADDRESS = '0x9C6B5FdC145912dfe6eE13A667aF3C5Eb07CbB89'
