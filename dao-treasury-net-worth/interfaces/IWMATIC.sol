57e93ffe030c7896601cfe594d71b830056a093a
MrSquiggles13 <ayresguitar@gmail.com>
2022-02-07 23:42:43 -0500

    Revert "Cant get v1.0 or v0 to work with events"
    This reverts commit 08290450af7b78fb307b616f7afcde993170a404.

--- dao-treasury-net-worth/interfaces/IWMATIC.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-interface IWMATIC {
-    function deposit() external payable;
-    function transfer(address to, uint value) external returns (bool);
-    function withdraw(uint) external;
-}
